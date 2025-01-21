3a034bb99b7d8865e3b6c0de5b04d801b43b0564
Giovanni Franco Calfiqueo <giovanni.franco@live.cl>
2025-01-21 12:23:43 -0300

    Funcionalidad completada

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //   let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
 
-amigos = [];
+const amigos = [];
+
+function numeroAzar(numeroMaximo) {
+    return Math.floor(Math.random()*numeroMaximo);
+} 
 
 function imprimirAmigos(amigo) {
-    listado = document.querySelector('#listaAmigos');
-    
-    nuevo = document.createElement("li");
+    const listado = document.querySelector('#listaAmigos');    
+    const nuevo = document.createElement("li");
     nuevo.innerHTML = amigo;
     listado.appendChild(nuevo);    
 }
 
 function agregarAmigo() {
-    inputAmigo = document.querySelector('#amigo');
+    const inputAmigo = document.querySelector('#amigo');
     if (inputAmigo.value && inputAmigo.value.trim().length > 0) {
-       amigos.push(inputAmigo);      
+       amigos.push(inputAmigo.value);      
        imprimirAmigos(inputAmigo.value);
        inputAmigo.value = '';
     } else {
-        alert("Debe ingresar un nombre de amigo valido.");
+       alert("Por favor, ingrese un nombre valido.");
     }
+}
+
+function sortearAmigo() {
+    const resultado = document.querySelector('#resultado');
+    const indiceAmigoSorteado = numeroAzar(amigos.length);
+    resultado.innerHTML = '';
+    const elegido = document.createElement("li");
+    elegido.innerHTML = "El amigo secreto sorteado es: " + amigos[indiceAmigoSorteado];
+    resultado.appendChild(elegido);
 }
