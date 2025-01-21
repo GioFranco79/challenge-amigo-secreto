5b62b30209fee75344674b27d8a3c6aa61899727
Giovanni Franco Calfiqueo <giovanni.franco@live.cl>
2025-01-21 12:46:11 -0300

    Validaciones agregadas

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//   let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+// Programador: Giovanni Franco Calfiqueo.
 
 const amigos = [];
 
+// Genera un numero al azar con un tope maximo
 function numeroAzar(numeroMaximo) {
     return Math.floor(Math.random()*numeroMaximo);
 } 
 
+// Revisa que un texto dado sea compuesto solo por letras mayusculas o minusculas y espacios entre palabras
+function soloLetras(texto) {    
+    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s]+$/;
+    return regex.test(texto);
+}
+
+// Imprime al nuevo amigo en pantalla
 function imprimirAmigos(amigo) {
     const listado = document.querySelector('#listaAmigos');    
     const nuevo = document.createElement("li");
@@ -14,17 +21,23 @@ function imprimirAmigos(amigo) {
     listado.appendChild(nuevo);    
 }
 
+// Agrega al nuevo amigo al arreglo solo si el nombre es valido
 function agregarAmigo() {
     const inputAmigo = document.querySelector('#amigo');
-    if (inputAmigo.value && inputAmigo.value.trim().length > 0) {
-       amigos.push(inputAmigo.value);      
-       imprimirAmigos(inputAmigo.value);
+    if (inputAmigo.value && inputAmigo.value.trim().length > 0 && soloLetras(inputAmigo.value)) {
+       // Solo si el amigo no esta en el arreglo lo agrega para no repetirlo 
+       if (!amigos.includes(inputAmigo.value)) {            
+            amigos.push(inputAmigo.value);
+            imprimirAmigos(inputAmigo.value);
+            console.log(amigos);
+       }
        inputAmigo.value = '';
     } else {
        alert("Por favor, ingrese un nombre valido.");
     }
 }
 
+// Sortea un amigo que este dentro del arreglo 'amigos' al azar y lo imprime en pantalla
 function sortearAmigo() {
     const resultado = document.querySelector('#resultado');
     const indiceAmigoSorteado = numeroAzar(amigos.length);
