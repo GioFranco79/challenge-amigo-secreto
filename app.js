34f1cec22eca17855546563a1807d2f2b79541dc
Giovanni Franco Calfiqueo <giovanni.franco@live.cl>
2025-01-21 12:03:38 -0300

    Agregando amigos resuelto

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //   let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+amigos = [];
+
+function imprimirAmigos(amigo) {
+    listado = document.querySelector('#listaAmigos');
+    
+    nuevo = document.createElement("li");
+    nuevo.innerHTML = amigo;
+    listado.appendChild(nuevo);    
+}
+
+function agregarAmigo() {
+    inputAmigo = document.querySelector('#amigo');
+    if (inputAmigo.value && inputAmigo.value.trim().length > 0) {
+       amigos.push(inputAmigo);      
+       imprimirAmigos(inputAmigo.value);
+       inputAmigo.value = '';
+    } else {
+        alert("Debe ingresar un nombre de amigo valido.");
+    }
+}
